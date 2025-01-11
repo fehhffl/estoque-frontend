5f10cff1154c9d47eb47deee5243e12a3279ff0f
fehhffl <fe.forioni@gmail.com>
2025-01-11 02:24:11 -0300

    Style the login screen

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -4,6 +4,8 @@ import {
   StyleSheet,
   SafeAreaView,
   KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
 } from "react-native";
 
 const Register = () => {
@@ -11,7 +13,23 @@ const Register = () => {
     <SafeAreaView style={styles.safeAreaView}>
       <KeyboardAvoidingView style={styles.container}>
         <View style={styles.topView}>
-          <Text style={styles.welcomeText}> Bem-Vindo</Text>
+          <Text style={styles.tittleText}>Bem-Vindo</Text>
+        </View>
+        <View style={styles.centerView}>
+          <TextInput style={styles.TextInput} placeholder="User"></TextInput>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Password"
+            secureTextEntry
+          ></TextInput>
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.borderButton}>
+            <Text style={styles.borderButtonText}>Logar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.borderButton}>
+            <Text style={styles.borderButtonText}>Registrar</Text>
+          </TouchableOpacity>
         </View>
       </KeyboardAvoidingView>
     </SafeAreaView>
@@ -25,13 +43,49 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 24,
+    justifyContent: "center",
+    gap: 80,
   },
   topView: {
     justifyContent: "flex-start",
     alignItems: "center",
+    marginBottom: 20,
   },
-  welcomeText: {
+  tittleText: {
     fontSize: 30,
+    fontWeight: "bold",
+  },
+  centerView: {
+    alignItems: "center",
+    gap: 20,
+  },
+  TextInput: {
+    borderRadius: 12,
+    borderColor: "gray",
+    borderWidth: 1,
+    padding: 10,
+    height: 40,
+    width: "100%",
+    marginBottom: 15,
+  },
+  buttonContainer: {
+    width: "100%",
+    alignItems: "center",
+  },
+  borderButton: {
+    borderWidth: 1,
+    borderColor: "gray",
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 8,
+    width: "80%",
+    alignItems: "center",
+    marginBottom: 15,
+  },
+  borderButtonText: {
+    color: "gray",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
 
